723c841a5dd2e526bc742b4011c50c796d0721b1
Rory <rory.fayed14@imperial.ac.uk>
2016-07-16 15:37:26 +0100

    functions added to googleMaps.js file

--- public/js/main.js
+++ public/js/main.js
@@ -7,20 +7,9 @@ $(function(){
 	$('.modal-trigger').leanModal();
 	$('.slider').slider({full_width: false});
 	Materialize.toast('I am a toast!', 3000);
-    initMap();
+   
 });
 
 
 
-function initMap() {
-    alert();
-var div = document.createElement("div");
-div.id= "map";
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    scrollwheel: false,
-    zoom: 8
-  });
-}
 
